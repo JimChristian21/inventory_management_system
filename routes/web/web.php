42788f553147afd03b740d1b12fa05a5ed7bde49
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2025-02-20 01:36:21 +0800

    migrate routes to its specific files

--- routes/web/web.php
+++ routes/web/web.php
@@ -1,32 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
+require __DIR__.'/home.php';
 require __DIR__.'/auth.php';
+require __DIR__.'/profile.php';
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('guest')->group(function() {
-
-    Route::get('/', function () {
-        return redirect()->route('login');
-    });
-});
-
-Route::middleware('auth')->group(function () {
-
-    Route::get('/', function () {
-        return redirect()->route('dashboard');
-    });
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 
