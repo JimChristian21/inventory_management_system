13c507e0e842f4b310758b0fa47b768b65063c09
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2025-02-20 01:42:20 +0800

    removed route files and moved back to main route

--- routes/web/web.php
+++ routes/web/web.php
@@ -1,8 +1,31 @@
 <?php
 
-require __DIR__.'/home.php';
-require __DIR__.'/auth.php';
-require __DIR__.'/profile.php';
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
+require __DIR__.'/auth.php';
+
+Route::middleware('guest')->group(function() {
+
+    Route::get('/', function () {
+        return redirect()->route('login');
+    });
+});
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->middleware(['verified'])->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/', function () {
+        return redirect()->route('dashboard');
+    });
+});
 
 
