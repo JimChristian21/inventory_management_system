4fca7d0d6823cd32d45da1e0e65b25a530deeb0f
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2025-02-22 22:52:04 +0800

    Added table

---
+++ resources/js/Components/Table/Paginator.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+
+    const props = defineProps({
+        from: Number,
+        to: Number,
+        total: Number,
+        links: Array
+    });
+</script>
+
+<template>
+    <div class="flex flex-row justify-between p-2 text-slate-500">
+        <div>
+            <template v-for="link in props.links">
+                <Link class="hover:bg-slate-500 p-2 rounded-lg" :href="link.url ?? '#'" as="button">{{ link.label }}</Link>
+            </template>
+        </div>
+        <div class="flex flex-row text-slate-500">
+            <div class="">
+                <span>Item per page: </span> 
+            </div>
+            <span>{{ props.from }} - {{ props.to }} of {{ props.total }}</span>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,10 +1,20 @@
 <script setup>
-    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
     import { Head } from '@inertiajs/vue3';
-
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import Button from "primevue/button";
+    import Paginator from '@/Components/Table/Paginator.vue';
+    
     const props = defineProps({
-        users: Array
+        users: Object
     });
+
+    const headers = [
+        'Name',
+        'Roles',
+        'Email',
+        'Created',
+        'Actions'
+    ];
 
 </script>
 
@@ -20,36 +30,56 @@
             </h2>
         </template>
 
-        <div class="py-12">
+        <div class="py-12 px-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div
                     class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
                 >
                     <div class="p-6 text-gray-900">
-                        <table>
+                        <table class="w-full">
                             <thead>
-                                <tr>
-                                    <td>Name</td>
-                                    <td>Role</td>
-                                    <td>Email</td>
-                                    <td>Created</td>
-                                    <td>Actions</td>
+                                <tr class="border border-black rounded-xl font-bold">
+                                    <td class="" v-for="header in headers">
+                                        {{ header }}
+                                    </td>
                                 </tr>
                             </thead>
 
                             <tbody>
-                                <tr v-for="user in props.users.data" :key="1">
+                                <tr 
+                                    v-for="user in props.users.data" 
+                                    :key="1"
+                                    class="border-b-2 border-slate-500 p-10"
+                                >
                                     <td>{{ user.name }}</td>
                                     <td>Roles</td>
                                     <td>{{ user.email }}</td>
                                     <td>{{ user.created_at }}</td>
-                                    <td>ACTIONS</td>
+                                    <td>
+                                        <Button>ACTIONS</Button>
+                                    </td>
                                 </tr>
                             </tbody>
-                        </table>>
+                        </table>
+                        <Paginator 
+                            :links="props.users.links" 
+                            :from="props.users.from" 
+                            :to="props.users.to"
+                            :total="props.users.total"
+                        />
                     </div>
                 </div>
             </div>
+            <!-- <DataTable :value="props.users.data" dataKey="id" tableStyle="min-width: 50rem" :totalRecords="props.users.total">
+                <Column field="name" header="Name"/>
+                <Column field="roles" header="Roles"/>
+                <Column field="email" header="Email"/>
+                <Column field="created_at" header="Created"/>
+                <Column field="actions" header="Actions">
+                    Test
+                </Column>
+                 <Column v-for="user of props.users.data" :key="user.id" :field="user.id" :header="user.id"></Column> -->
+            <!-- </DataTable> -->
         </div>
     </AuthenticatedLayout>
 </template>
